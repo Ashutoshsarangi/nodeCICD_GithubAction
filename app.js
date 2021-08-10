4e746ee437973d7ccd7962863a87b4a5d2f24b68
Ashu <ashutoshsarangi95@gmail.com>
2021-08-10 11:21:42 +0530

    It will pich port from Env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app  = express();
 app.get('/', (req, res)=>{
     res.send('Hello World');
 });
-var port_number = server.listen(process.env.PORT || 3000);
+var port_number = (process.env.PORT || 3000);
 
 app.listen(port_number, ()=> console.log('Port os Running on ' + port_number));
 
