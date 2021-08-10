81405fca63529ac964c65bc516c0c05590a29571
Ashu <ashutoshsarangi95@gmail.com>
2021-08-10 09:17:15 +0530

    Check for the Solved

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express  =  require('express');
 const app  = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello World 12312');
+    res.send('Hello World');
 });
 
 app.listen(3000, ()=> console.log('Port os Running on 3000'));
